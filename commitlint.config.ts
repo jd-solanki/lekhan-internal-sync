c34bee8e6d8a135209712639936586da5b07fa08
JD Solanki <jdsolanki0001@gmail.com>
2025-08-29 10:53:22 +0530

    chore: add GitHub Actions extension to VSCode recommendations

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,14 +2,6 @@ import type { UserConfig } from '@commitlint/types'
 
 const config = {
   extends: ['@commitlint/config-conventional'],
-  /* Docs: https://commitlint.js.org/reference/rules-configuration.html */
-  rules: {
-    'subject-case': [
-      2, /* 0 => disable rule, 1 => warning & 2 => error */
-      'always',
-      ['lower-case'],
-    ],
-  },
 } satisfies UserConfig
 
 export default config
