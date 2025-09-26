9ea3723281de4e16ae850494d41db3b518a8032d
JD Solanki <jdsolanki0001@gmail.com>
2025-09-26 10:46:28 +0530

    chore: update comments in banner store for clarity on persistence and requirements

--- app/stores/banner.ts
+++ app/stores/banner.ts
@@ -1,5 +1,7 @@
 import type { BannerProps } from '@nuxt/ui'
 
+// You can persist this as well with help of cookie
+// Update code as per your business requirements
 export const useBannerStore = defineStore('banner', () => {
   const props = reactive<BannerProps>({})
 
