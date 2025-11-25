948a9a57aebe59b1439e1bdc2386f549131dc12e
JD Solanki <jdsolanki0001@gmail.com>
2025-11-25 11:44:50 +0530

    feat: improve website header

--- layers/docs/components/website/Header.vue
+++ layers/docs/components/website/Header.vue
@@ -1,19 +1,65 @@
 <script lang="ts" setup>
+import type { NavigationMenuItem } from '@nuxt/ui'
+
 const runtimeConfig = useRuntimeConfig()
+
+const userStore = useUserStore()
+const paymentsStore = usePaymentsStore()
+
+const items = computed<NavigationMenuItem[]>(() => [
+  {
+    label: 'Documentation',
+    to: '/docs',
+    target: '_blank',
+  },
+  {
+    label: 'Changelog',
+    to: '/docs/changelog',
+    target: '_blank',
+  },
+  // {
+  //   label: 'Blog',
+  //   to: '/blog',
+  // target: '_blank',
+  // },
+])
+
+const { data: products } = await useFetch('/api/polar/products')
+const product = computed(() => products.value?.result.items[0])
+
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'No products found',
+  })
+}
+
+const hasPurchasedProduct = await paymentsStore.hasPurchasedProduct(product.value.id)
 </script>
 
 <template>
   <UHeader
     :title="runtimeConfig.public.app.name"
     to="/"
+    :ui="{ right: 'gap-3' }"
   >
+    <UNavigationMenu :items="items" />
     <template #right>
-      <ClientOnly>
-        <UColorModeButton />
-        <template #fallback>
-          <div class="h-8 w-8 animate-pulse bg-neutral-200 dark:bg-neutral-800 rounded-md" />
-        </template>
-      </ClientOnly>
+      <LayoutDefaultUserDropdown v-if="userStore.user">
+        <!-- Trigger -->
+        <UAvatar
+          :src="userStore.avatarUrl"
+          :alt="userStore.user?.name"
+          :text="getInitials(userStore.user?.name)"
+        />
+      </LayoutDefaultUserDropdown>
+      <UButton
+        v-if="!hasPurchasedProduct"
+        loading-auto
+        @click="paymentsStore.createCheckoutSession(product!.id)"
+      >
+        Get LaunchDayOne
+      </UButton>
     </template>
   </UHeader>
 </template>
