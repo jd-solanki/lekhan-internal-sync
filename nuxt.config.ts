1e20b03abd0bbbce6f6a3e18a76e0fcc62ffe668
JD Solanki <jdsolanki0001@gmail.com>
2026-01-10 23:08:26 +0530

    feat: remove logo as icon as we now rely on png for logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,16 +12,6 @@ export default defineNuxtConfig({
     '/polar/customer-portal': { robots: false },
     '/polar/success': { robots: false },
   },
-  // TODO: Avoid copying public `logo.svg` to `app/assets/icons/app`
-  // Issue URL: https://github.com/LaunchDayOne/LaunchDayOne/issues/122
-  icon: {
-    customCollections: [
-      {
-        prefix: 'app',
-        dir: './app/assets/icons/app',
-      },
-    ],
-  },
   app: {
     // pageTransition: { name: 'page-transition-zoom-fade', mode: 'out-in' },
     head: {
