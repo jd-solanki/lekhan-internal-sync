d358c3e7846667794774d5d9a3f1ac0273965438
JD Solanki <jdsolanki0001@gmail.com>
2026-02-06 19:35:52 +0530

    feat: add support for importing snippets from source code

--- layers/01.base/nuxt.config.ts
+++ layers/01.base/nuxt.config.ts
@@ -1,5 +1,6 @@
 import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import remarkImportSnippetPlugin from './content/plugins/importSnippet'
 
 const currentDir = dirname(fileURLToPath(import.meta.url))
 
@@ -7,4 +8,16 @@ export default defineNuxtConfig({
   css: [
     join(currentDir, './app/assets/css/transitions.css'),
   ],
+  content: {
+    build: {
+      markdown: {
+        remarkPlugins: {
+          // Because Nuxt Content loads plugins dynamically we need to pass absolute path
+          [join(currentDir, './content/plugins/importSnippet.ts')]: {
+            instance: remarkImportSnippetPlugin,
+          },
+        },
+      },
+    },
+  },
 })
