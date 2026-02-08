bd4e33404dc6bc480a5e17c386551ec760fc8817
JD Solanki <jdsolanki0001@gmail.com>
2026-02-08 21:38:47 +0530

    feat: add initial implementation for server API testing

---
+++ tests/utils/fetch.ts
@@ -0,0 +1,15 @@
+import type { InternalApi } from 'nitropack/types'
+import type { FetchOptions } from 'ofetch'
+import type {} from '../../.nuxt/types/nitro-routes'
+import { $fetch } from 'ofetch'
+
+const port = process.env.NUXT_PORT || 3000
+
+export async function apiFetch(
+  path: keyof InternalApi,
+  options?: FetchOptions,
+) {
+  return $fetch.create({
+    baseURL: `http://localhost:${port}`,
+  })(path as string, options)
+}
