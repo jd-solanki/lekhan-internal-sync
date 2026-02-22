dc4b9bc97ee5b5f3f553acd652427d807816822b
JD Solanki <jdsolanki0001@gmail.com>
2026-02-22 16:53:25 +0530

    refactor: simplify nuxt ui config

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,5 @@
-import { defu } from 'defu'
-import { outlineCardWOBorders } from '../layers/01.base/config/nuxt-ui'
-
 export default defineAppConfig({
   app: {
     logo: '/logo.png',
   },
-  ui: defu({
-    /* Write your Nuxt UI config */
-  }, outlineCardWOBorders),
 })
--- layers/01.base/app/app.config.ts
+++ layers/01.base/app/app.config.ts
@@ -17,6 +17,14 @@ export default defineAppConfig({
     },
   ],
   ui: {
+    card: {
+      slots: {
+        root: 'py-4 sm:*:py-6 divide-none gap-6 flex flex-col',
+        header: 'py-0!',
+        body: 'py-0!',
+        footer: 'py-0!',
+      },
+    },
     table: {
       slots: {
         thead: 'text-nowrap',
