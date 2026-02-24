a8174f626cc4e8f71a91cd1b9bbc0390b2079ff8
JD Solanki <jdsolanki0001@gmail.com>
2026-02-24 10:01:36 +0530

    fix: update brand color in emails

--- layers/email/shared/utils/constants.ts
+++ layers/email/shared/utils/constants.ts
@@ -1,2 +1,2 @@
 // Grab HEX code from https://tailwindcss.com/docs/colors
-export const BRAND_COLOR = '#000' // (black) Ensure it's in sync with your app primary color
+export const BRAND_COLOR = '#3b82f6' // (blue 500) Ensure it's in sync with your app primary color
